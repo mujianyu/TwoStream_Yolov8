ddd5cbbcc39eab02a4a844f651a5f1199c1f0df9
mujianyu <2559405752@qq.com>
2024-05-30 09:20:13 +0800

    更新

--- OBBtest.py
+++ OBBtest.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
  
  
-model = YOLO('/home/mjy/ultralytics/yaml/yolov8n-obb.yaml').load('/home/mjy/ultralytics/runs/obb/train2/weights/best.pt')  # build from YAML and transfer weights
+model = YOLO('/home/mjy/ultralytics/yaml/PKICBAMyolov8n-obb.yaml').load('/home/mjy/ultralytics/runs/obb/PKICBAM/weights/best.pt')  # build from YAML and transfer weights
 
 
-metrics = model.val(data='/home/mjy/ultralytics/data/coco81.yaml',imgsz=(736,864))
+metrics = model.val(data='/home/mjy/ultralytics/data/coco81.yaml',imgsz=640)
--- OBBtrain.py
+++ OBBtrain.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
  
  
-model = YOLO('/home/mjy/ultralytics/yaml/PKIICBAMRyolov8n-obb.yaml') # build from YAML and transfer weights
+model = YOLO('/home/mjy/ultralytics/yaml/PKISAyolov8n-obb.yaml') # build from YAML and transfer weights
 
 # Train the model
 results = model.train(data='/home/mjy/ultralytics/data/coco8.yaml', epochs=100, imgsz=640,resume=False)
