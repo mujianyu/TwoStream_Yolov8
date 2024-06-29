6fe8ba67ec6e238c5182d4b931c991c80c7e1795
mujianyu <2559405752@qq.com>
2024-06-29 10:06:52 +0800

    dsa

--- OBBtrain.py
+++ OBBtrain.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO
  
  
-model = YOLO('/home/mjy/ultralytics/yaml/PPAyolov8n-obb.yaml')
+model = YOLO('/home/mjy/ultralytics/yaml/yolov8n.yaml')
 # .load('/home/mjy/ultralytics/yolov8n-obb.pt ') # build from YAML and transfer weights
 
 # Train the model
-results = model.train(data='/home/mjy/ultralytics/data/coco8.yaml',batch=16,epochs=100)
+results = model.train(data='/home/mjy/ultralytics/data/drone.yaml',batch=16,epochs=100)
