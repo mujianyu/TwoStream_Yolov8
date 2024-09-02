cae11dce504d31aaafcff0b50c0e9710cbebf055
mujianyu <2559405752@qq.com>
2024-09-02 13:07:26 +0800

    更新

--- OBBtrain.py
+++ OBBtrain.py
@@ -1,7 +1,5 @@
 from ultralytics import YOLO
 import ultralytics.nn.tasks
- 
-model = YOLO('./yaml/Fasteryolov8n.yaml')
 
-# Train the model
+model = YOLO('./yaml/Fasteryolov8n.yaml')
 results = model.train(data='/home/mjy/ultralytics/data/drone2.yaml',batch=16,epochs=200)
